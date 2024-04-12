6573ac35fe1c43e17c03c43b163e5eb11ac4601d
Brenno <brennoantonio1221@gmail.com>
2024-04-12 18:51:14 -0400

    portfolio

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,23 @@
   <section id="Home">
    
     <div class="titulo-container">
-      <h1 class="titulo">Brenno Santos</h1>
-      <h2 class="sub-titulo">Futuro Full-Stack <br>Developer</h2>
+      <h1 class="titulo" v-motion-slide-left :delay="300">Brenno Santos</h1>
+      <h2 class="sub-titulo" v-motion-slide-right :delay="200">Futuro Full-Stack <br>Developer</h2>
     </div>
     <TopMenu/>
    
   </section>
     <section id="Sobre">
-     asdadas
+     <div class="sobre-container">
+     <h1 class="sobre-titulo" v-motion-slide-visible-left :delay="300">Estudo Análise e Desenvolvimento de Sistemas na FATEC- SENAI e estou
+       trilhando meu caminho para me tornar um programador Full-Stack. Busco aventuras e desafios 
+       no mundo do Front-End para mostrar minhas habilidades de design e código e explorar todo meu 
+       potencial. Confira meu portfólio e vamos criar juntos!</h1>
+       <button class="sobre-btn">Saiba mais sobre mim</button>
+     </div>
+    
     </section>
+
     <section id="Projetos">
 asd
     </section>
@@ -24,22 +32,21 @@ asd
 </template>
 <script >
 
-import TopMenu from '@/components/TopMenu.vue';
 
+import TopMenu from './components/TopMenu.vue';
    
 
 export default {
   name: 'App',
   components: {
     TopMenu,
-  
   },
-  
 }
 </script>
 <style>
 html {
   scroll-behavior: smooth;
+  overflow-x: hidden;
 }
 body {
   background-color: rgb(67, 19, 103);
@@ -48,22 +55,11 @@ body {
   font-family: "Bebas Neue", sans-serif;
   font-weight: 400;
   font-style: normal;
-
-}
-
-@media screen and (max-width: 600px) {
-  section {
-    height: 200vw;
-  }
-  #Home {
-    min-height: 300vw;
-  }
 }
 #Home {
   min-height: 50vw;
   color: aliceblue;
   background-color: rgb(67, 19, 103);
-
 }
 #Home .titulo-container { 
  display: flex;
@@ -71,25 +67,52 @@ body {
  margin: 3rem;
 }
 #Home .titulo {
-  font-size: 3rem;
-  transform: translateX(0px);
-  animation: titulo 1s ease-in-out;
+  font-size: 5rem;
+  
  
 }
-#Home .sub-titulo {
-  transform: translateY(-40px);
-  animation: sub-titulo 1.5s ease-in-out;
+@media screen and (max-width: 600px) {
+ 
+  #Home .titulo-container { 
+    margin: 2rem;
+   font-size: 1rem;
+   }
+   #Home .titulo {
+    font-size: 3rem;
+  }
+  #Home {
+    min-height: 200vw;
+  }
+  #Sobre{
+    min-height: 200vw;
+  }
+  body {
+    overflow-x: hidden;
+  }
 }
-@keyframes titulo {
-  0% {opacity: 0;
-    transform: translateX(-150px);}
-    50% {opacity: 0; }
-    100% {opacity: 1;}
-    }
-@keyframes sub-titulo {
-  0% {opacity: 0;
-    transform: translateY(100px);}
-    50% {opacity: 0;}
-    100% {opacity: 1;}
-    }
+
+  #Sobre{
+    background-color: rgb(157, 157, 157);
+    height: 100vw;
+  }
+  #Sobre .sobre-container {
+    display: flex;
+    flex-direction: column;
+    margin: 4rem;
+  }
+  #Sobre .sobre-titulo {
+    display: flex;
+    margin-top: 12rem;
+    width: 60%;
+    font-size: 1.2rem;
+  }
+  #Sobre .sobre-btn {
+    width: 12rem;
+    height: 4rem;
+    color: aliceblue;
+    background-color: rgb(47, 47, 47);
+    margin: 4rem;
+    box-shadow: 0px 0px 10px;
+    border-radius: 0.4rem;
+  }
 </style>
