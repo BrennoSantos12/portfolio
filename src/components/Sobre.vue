c4e0f0e790b942dc1730b5b0cc68b70fcb6b2a51
Brenno <brennoantonio1221@gmail.com>
2024-08-24 13:52:20 -0400

    portfolio

--- src/components/Sobre.vue
+++ src/components/Sobre.vue
@@ -1,52 +1,58 @@
 <template>
-    <div class="sobre-container">
-      <h1 class="sobre-mim">SOBRE MIM</h1>
-        <h1 class="sobre-titulo" >
-         Estudo Análise e Desenvolvimento de Sistemas na FATEC- SENAI e estou
-          trilhando meu caminho para me tornar um programador com skills em <span class="span">Front-End</span> e <span class="span">Back-end</span>.
-           Busco aventuras e desafios 
-          no mundo do <span class="span">Front-End</span> para mostrar minhas habilidades de design e código e explorar todo meu 
-          potencial. <br> Possuo habilidades em: <span class="span">HTML5, CSS3, javascript, Python, Node.js e Vue.js.</span>
-          Confira meu portfólio e vamos trabalhar juntos!</h1>
-   
-          <a class="button" href="../files/CV- Brenno Santos.pdf">Baixe o meu Currículo</a>
-        </div>
+  <div class="sobre-container">
+    <h1 class="sobre-mim">SOBRE MIM</h1>
+    <h1 class="sobre-titulo">
+      Estudo Análise e Desenvolvimento de Sistemas na FATEC- SENAI e estou
+      trilhando meu caminho para me tornar um programador com skills em <span class="span">Front-End</span> e <span
+        class="span">Back-end</span>.
+      Busco aventuras e desafios
+      no mundo do <span class="span">Front-End</span> para mostrar minhas habilidades de design e código e explorar todo
+      meu
+      potencial. <br> Possuo habilidades em: <span class="span">HTML5, CSS3, javascript, Python, Node.js e
+        Vue.js.</span>
+      Confira meu portfólio e vamos trabalhar juntos!</h1>
+
+    <a class="button" href="../files/CV- Brenno Santos.pdf">Baixe o meu Currículo</a>
+  </div>
 
 </template>
 
 <style>
-
-#Sobre{
+#Sobre {
   z-index: 1;
   position: relative;
-    color: var(--letra-cor);
-    background-color: var(--secundary-color);
-    height: 60vw;
-    display: flex;
-    align-items: center;
-  }
-  .sobre-mim {
-    font-size: 2rem;
-  }
-#Sobre .sobre-container {
-   display: flex;
-   flex-direction: column;
-   align-items: center;
-   text-align: center;
-   gap: 2rem;
+  color: var(--letra-color);
+  background-color: var(--secundary-color);
+  height: 60vw;
+  display: flex;
+  align-items: center;
 }
+
+.sobre-mim {
+  font-size: 2rem;
+}
+
+#Sobre .sobre-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  gap: 2rem;
+}
+
 #Sobre .sobre-titulo {
   font-size: 1.2rem;
   width: 40%;
   overflow-wrap: break-word;
-  color: var(--letra-cor);
+  color: var(--letra-color);
 }
+
 .span {
   color: yellow;
 }
+
 .button {
   text-decoration: none;
-  --color: #6300a9;
   padding: 1.3em 6em;
   background-color: transparent;
   border-radius: 0.3em;
@@ -58,11 +64,11 @@
   font-size: 17px;
   border: 1px solid;
   text-transform: uppercase;
-  color: var(--color);
+  color: var(--button-sobre-color);
   z-index: 1;
- }
- 
-  .button::after {
+}
+
+.button::after {
 
   content: '';
   display: block;
@@ -72,54 +78,56 @@
   position: absolute;
   border-radius: 50%;
   z-index: -1;
-  background-color: var(--color);
+  background-color: var(--button-sobre-color);
   transition: 1.1s ease;
- }
+}
 
- .button::after {
+.button::after {
   left: calc(50% + 1em);
   top: calc(200% + 1em);
- }
- 
- .button:hover::before, .button:hover::after {
+}
+
+.button:hover::before,
+.button:hover::after {
   height: 510px;
   width: 610px;
- }
- 
- .button:hover {
+}
+
+.button:hover {
   color: rgb(74, 2, 74);
   filter: brightness(0.9);
- }
+}
 
- @media screen and (max-width: 900px) {
-       
+@media screen and (max-width: 900px) {
+
   #Sobre {
     height: 95vh;
   }
+
   #Sobre .sobre-titulo {
     font-size: 0.8rem;
     width: 80%;
     overflow-wrap: break-word;
   }
-  .button { 
+
+  .button {
 
     padding: 1em 2em;
   }
- }
-
+}
 </style>
 
 <script>
 export default {
-    name: 'SobreVue',
-   mounted() {
-    
+  name: 'SobreVue',
+  mounted() {
 
-    }
+
   }
+}
 
 
 
 
-    
+
 </script>
