cce53508bdaa2755072e007f42b2d2a99b84d7e6
BrennoSantos12 <brennoantonios@gmail.com>
2025-11-11 11:25:35 -0400

    salvando parte 1 da cortina

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,37 +1,31 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="p-4 text-white flex items-center justify-between">
-    <div class="bloco group font-oswald font-bold cursor-pointer">
-      <div class="text-up flex items-center gap-2">
-        OPEN TO WORK
-        <div class="h-2 w-2 rounded-full bg-green-500 animate-pulse" />
-      </div>
-      <h1 class="text-bt text-yellow-500">LET'S TALK!</h1>
-    </div>
-    <div class="bloco group text-2xl font-oswald font-bold">
+  <div class="p-2 flex items-center justify-between">
+    <div class="bloco group font-oswald font-bold text-5xl">
       <h1 class="text-up">
         <span>BRENNO </span>
-        <span>SANTOS</span>
+        <span class="text-amber-400">SANTOS</span>
       </h1>
-      <h1 class="text-bt text-purple-400/50">
-        <span>BRENNO </span>
+      <h1 class="text-bt">
+        <span class="text-amber-400">BRENNO </span>
         <span>SANTOS</span>
       </h1>
     </div>
-
-    <div class="flex gap-2 text-[14px] font-oswald font-bold cursor-pointer">
+    <div
+      class="flex flex-col text-end text-2xl font-oswald font-bold cursor-pointer"
+    >
       <div class="bloco group">
         <span class="text-up">SOBRE</span>
-        <span class="text-bt text-yellow-500">SOBRE</span>
+        <span class="text-bt text-amber-400">SOBRE</span>
       </div>
       <div class="bloco group">
         <span class="text-up">PROJETOS</span>
-        <span class="text-bt text-yellow-500">PROJETOS</span>
+        <span class="text-bt text-amber-400">PROJETOS</span>
       </div>
       <div class="bloco group">
-        <span class="text-up">SERVIÇOS</span>
-        <span class="text-bt text-yellow-500">SERVIÇOS</span>
+        <span class="text-up">MEUS SERVIÇOS</span>
+        <span class="text-bt text-amber-400">MEUS SERVIÇOS</span>
       </div>
     </div>
   </div>
